034f7aa7eadedb02719793f42c23f4216ee824a8
vicferarb <vicferarb@gmail.com>
2022-10-28 11:15:35 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:rickmorty/services/reqres_services.dart';
+import 'package:characters/characters.dart';
+import 'package:rickmorty/models/caracter_model.dart';
+
+void main(List<String> args) {
+  ReqResServices().getCharacters();
+  //print('name');
+}
---
+++ lib/services/reqres_services.dart
@@ -0,0 +1,9 @@
+import 'package:characters/characters.dart';
+import 'package:rickmorty/models/caracter_model.dart';
+
+class ReqResServices {
+  getCharacters() {
+    print("Name");
+    print('gender');
+  }
+}
